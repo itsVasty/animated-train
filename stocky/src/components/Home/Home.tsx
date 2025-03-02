633c0898a5ad8d359feace496441371a4b60232f
Vasty Muaku <vasty@wethinkcode.co.za>
2025-03-02 21:02:04 +0200

    added dummy stock data

--- stocky/src/components/Home/Home.tsx
+++ stocky/src/components/Home/Home.tsx
@@ -6,6 +6,7 @@ interface HomeProps {}
 const Home: FC<HomeProps> = () => (
   <div className={styles.Home}>
     <h1>Home</h1>
+    <p>Welcome to Mack Stock, the only place to get information on your stocks!</p>
   </div>
 );
 
--- stocky/src/components/Market/Market.tsx
+++ stocky/src/components/Market/Market.tsx
@@ -1,12 +1,59 @@
 import React, { FC } from 'react';
 import styles from './Market.module.css';
 
+const STOCKS = [
+  {currency: "USD", buyingPrice: "R16.40", sellingPrice: "R17.40"},
+  {currency: "GBP", buyingPrice: "R20.12", sellingPrice: "R22.40"}
+];
+
 interface MarketProps {}
 
-const Market: FC<MarketProps> = () => (
-  <div className={styles.Market}>
-    <h1>Market</h1>
-  </div>
-);
+function StockTable({stocks} : any){
+  const rows : any = []
+  stocks.forEach((stock : any) => {
+    rows.push(<StockRow stock={stock}/>)
+  })
+
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Cuurency</th>
+          <th>Buying Price</th>
+          <th>Selling Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+function StockRow({stock} : any){
+  return(
+    <tr>
+      <td>
+        {stock.currency}
+      </td>
+      <td>
+        {stock.buyingPrice}
+      </td>
+      <td>
+        {stock.sellingPrice}
+      </td>
+    </tr>
+  )
+}
+
+
+
+const Market: FC<MarketProps> = () => {
+
+  return (
+    <div className={styles.Market}>
+      <h1>Market</h1>
+      <StockTable stocks={STOCKS}/>
+    </div>
+  )
+}
 
 export default Market;
